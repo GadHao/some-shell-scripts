09ad80a16b5fd6ecc785894491718c4a1c442505
KamenRiderKuuga <524472212@qq.com>
2022-06-13 01:41:18 +0800

    feat: get-localLization-strings-from-csharp-code

--- powershell/get-localLization-strings-from-csharp-code.ps1
+++ powershell/get-localLization-strings-from-csharp-code.ps1
@@ -1,15 +1,29 @@
-function Get-LocalLizationSQL {
+function Get-LocalLizationText {
     $allFiles = Get-ChildItem -Recurse -File -Filter '*.cs' | Where-Object { ($_.FullName -notlike '*\obj\*') -and ($_.FullName -notlike '*\bin\*') };
 
     foreach ($file in $allFiles) {
-        $result = ""
+        $result = @()
         Select-String -Pattern 'localizer\["(.+)"\]' -AllMatches -Path $file.FullName |
         ForEach-Object {
             if ($_.Matches.Groups[1].Value) {
                 $result += $_.Matches.Groups[1].Value
             }
         }
+
+        $result = $result | Sort-Object | Get-Unique
     }
     
-    $result
+    $result -join "`r`n" | Set-Clipboard
+}
+
+function Get-LocalLizationSQL {
+    $translatedText = Get-Clipboard
+    $translatedItems = $translatedText -split "`r`n"
+    $result = ''
+    foreach ($item in $translatedItems) {
+        $value = $item.Replace("'", "''")
+        $result += "INSERT OR IGNORE INTO LocalizationRecords(Key, ResourceKey, Text, LocalizationCulture, UpdatedTimestamp) VALUES('', '', '$value', 'en-US', '$((Get-Date).ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss.fffffff"))');`r`n"
+    }
+    
+    $result | Set-Clipboard
 }
